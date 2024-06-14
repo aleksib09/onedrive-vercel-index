a2d7b8cfa6f42925d312838a3a691b738010c95b
Alex Rendell <90443572+aleksib09@users.noreply.github.com>
2024-06-14 12:21:36 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'Instagram',
+      name: 'Website',
       link: 'https://inceptionracing.us',
     },
   ],
